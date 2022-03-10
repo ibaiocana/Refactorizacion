41cd0e6a7ae79148fc1f69257dc4d4dfae9c11e3
ibaiocana <iocana27@ieszaidinvergeles.org>
2022-03-10 13:30:40 +0100

    Solucion errores

--- Refactorizacion/src/refactorizacion/Refactorizacion.java
+++ Refactorizacion/src/refactorizacion/Refactorizacion.java
@@ -2,7 +2,16 @@ package refactorizacion;
 
 public class Refactorizacion {
     public static void main(String[] args) {
-
+        
+        int[] v = new int[5];
+        v[0] = 2;
+        v[1] = 4;
+        v[2] = 6;
+        Prueba p = new Prueba();
+        p.va = 10;
+        p.mo(v);
+        p.bo(v);
+        p.in(v);
     }
 
         
